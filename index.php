dfdd5e0884c65b025fdb139e456d2e3332fb6ed3
José Antonio Cordero Daniel <114194439+JackCorde@users.noreply.github.com>
2022-09-29 23:13:58 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alkalami&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h5 class="center-align" style="font-size: xxx-large; font-family: 'Alkalami', serif;">MIAU CROCHET</h5>
+    <nav class="nav-extended">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo"><img src="img/logo.png" class="circle responsive-img" alt="" width="30%" ></a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="medium material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="index.php" style="font-size: x-large;">Inicio</a></li>
+                <li><a href="#anim" style="font-size: x-large;">Animals Crochet</a></li>
+                <li><a href="#flor" style="font-size: x-large;">Flowers Crochet</a></li>
+            </ul>
+        </div>
+
+        
+    </nav>
+    <div class="container">
+        <br><br>
+        <div class="row">
+                <div class="col s6" style="background-color: #FDC430; border-radius:5%; padding:1em; border: solid white;">
+                    <div class="row">
+                        <div class="col s4">
+                            <img src="img/flor.webp" alt="" width="120%">
+                        </div>
+                        <div class="col s7" style="margin: .5em;">
+                            <br>
+                            <h5 class="left-align white-text">Flowers Crochet</h5><br>
+                            <a class="waves-effect waves-light btn white black-text" href="#flor">More Designs</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s6" style="background-color: #E9446F; border-radius:5%; padding:1em; border: solid white;">
+                    <div class="row">
+                        <div class="col s7 right-align" style="margin: .5em;">
+                            <br>
+                            <h5 class="right-align white-text">Animals Crochet</h5><br>
+                            <a class="waves-effect waves-white btn white black-text" href="#anim">More Designs</a>
+                        </div>
+                        <div class="col s4">
+                            <img src="img/abeja.png" alt="" width="100%">
+                        </div>
+                    </div>
+                </div>    
+        </div>
+
+        <h4 id="anim">Animals Crochet</h4>
+        <div class="row">
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/1.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Abejita</span>
+                        <h5><a href="#">$65</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/2.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Ranita</span>
+                        <h5><a href="#">$55</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/3.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Ajolote</span>
+                        <h5><a href="#">$130</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/4.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Mantarralla</span>
+                        <h5><a href="#">$85</a></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h4 id="flor">Flowers Crochet</h4>
+        <div class="row">
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/5.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo con 6 Tulipanes</span>
+                        <h5><a href="#">$280</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/6.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo Girasol y 3 Tulipanes</span>
+                        <h5><a href="#">$190</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/7.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo de Tulipan y 3 Mini Girasoles</span>
+                        <h5><a href="#">$100</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/8.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo 3 lavandas y 2 Mini Girasoles</span>
+                        <h5><a href="#">$110</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/9.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo de 3 Tulipanes</span>
+                        <h5><a href="#">$150</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/10.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo de Girasol y 3 Mini Girasoles</span>
+                        <h5><a href="#">$150</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/11.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo de Girasol y 3 Lavandas</span>
+                        <h5><a href="#">$110</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/12.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo 2 Margaritas y 3 Lavandas</span>
+                        <h5><a href="#">$120</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/13.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Lavandas, Rosas, Tulipán y Marga...</span>
+                        <h5><a href="#">$300</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/14.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Ramo de 2 Rosas y Tulipán</span>
+                        <h5><a href="#">$140</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/15.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Girasol, Rosas, Tulipán, Margarita...</span>
+                        <h5><a href="#">$280</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/16.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Económico Ramo de Tulipan</span>
+                        <h5><a href="#">$50</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3"></div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/17.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Económico Ramo de Rosa</span>
+                        <h5><a href="#">$60</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/18.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Económico Ramo de Girasol</span>
+                        <h5><a href="#">$60</a></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
+            <div class="card-panel grey lighten-5 z-depth-1">
+                <div class="row valign-wrapper">
+                    <div class="col s2">
+                        <img src="img/logo.png" alt="" class="circle responsive-img" width="40%">
+                    </div>
+                    <div class="col s10">
+                        <span class="black-text">
+                            Si quieres un diseño personalizado, buscame en mis redes sociales o mandame un mensaje por WhatsApp.
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <!--Lineas de conexion a la libreria de materialize-->
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
