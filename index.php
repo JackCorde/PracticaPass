80585b767b5194aa109516a01a3bddd5e5319963
José Antonio Cordero Daniel <114194439+JackCorde@users.noreply.github.com>
2022-10-13 20:19:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,34 +8,41 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Alkalami&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Document</title>
 </head>
 <body>
-    
     <h5 class="center-align" style="font-size: xxx-large; font-family: 'Alkalami', serif;">MIAU CROCHET</h5>
     <nav class="nav-extended">
         <div class="nav-wrapper">
             <a href="#" class="brand-logo"><img src="img/logo.png" class="circle responsive-img" alt="" width="30%" ></a>
-            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="medium material-icons">menu</i></a>
+            <a href="#mobile-demo" data-target="mobile-demo" class="sidenav-trigger"><i class="medium material-icons">menu</i></a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="index.php" style="font-size: x-large;">Inicio</a></li>
                 <li><a href="#anim" style="font-size: x-large;">Animals Crochet</a></li>
                 <li><a href="#flor" style="font-size: x-large;">Flowers Crochet</a></li>
             </ul>
         </div>
-
-        
     </nav>
+
+    <ul class="sidenav" id="mobile-demo">
+        <li><a href="index.php">Inicio</a></li>
+        <li><a href="#anim">Animals Crochet</a></li>
+        <li><a href="#flor">Flowers Crochet</a></li>
+    </ul>
+
     <div class="container">
         <br><br>
         <div class="row">
                 <div class="col s6" style="background-color: #FDC430; border-radius:5%; padding:1em; border: solid white;">
                     <div class="row">
-                        <div class="col s4">
+                        <div class="col l4">
                             <img src="img/flor.webp" alt="" width="120%">
                         </div>
-                        <div class="col s7" style="margin: .5em;">
+                        <div class="col l7" style="margin: .5em;">
                             <br>
                             <h5 class="left-align white-text">Flowers Crochet</h5><br>
                             <a class="waves-effect waves-light btn white black-text" href="#flor">More Designs</a>
@@ -44,12 +51,12 @@
                 </div>
                 <div class="col s6" style="background-color: #E9446F; border-radius:5%; padding:1em; border: solid white;">
                     <div class="row">
-                        <div class="col s7 right-align" style="margin: .5em;">
+                        <div class="col l7 right-align" style="margin: .5em;">
                             <br>
                             <h5 class="right-align white-text">Animals Crochet</h5><br>
                             <a class="waves-effect waves-white btn white black-text" href="#anim">More Designs</a>
                         </div>
-                        <div class="col s4">
+                        <div class="col l4">
                             <img src="img/abeja.png" alt="" width="100%">
                         </div>
                     </div>
@@ -83,11 +90,22 @@
             <div class="col l3">
                 <div class="card">
                     <div class="card-image waves-effect waves-block waves-light">
-                        <img class="activator" src="img/3.png">
+                        <img class="activator" src="img/pato.jpeg">
                     </div>
                     <div class="card-content center-align">
-                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Ajolote</span>
-                        <h5><a href="#">$130</a></h5>
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Patito</span>
+                        <h5><a href="#">$100</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/medusa.jpeg">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Medusa</span>
+                        <h5><a href="#">$60</a></h5>
                     </div>
                 </div>
             </div>
@@ -99,6 +117,50 @@
                     <div class="card-content center-align">
                         <span class="card-title activator grey-text text-darken-4">Amigurumi de Mantarralla</span>
                         <h5><a href="#">$85</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/3.png">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Ajolote</span>
+                        <h5><a href="#">$130</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/ajolote.jpeg">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Ajolote 2</span>
+                        <h5><a href="#">$50</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/abejita.jpeg">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Abeja</span>
+                        <h5><a href="#">$5</a></h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="img/cactus.jpeg">
+                    </div>
+                    <div class="card-content center-align">
+                        <span class="card-title activator grey-text text-darken-4">Amigurumi de Cactus</span>
+                        <h5><a href="#">$120</a></h5>
                     </div>
                 </div>
             </div>
@@ -280,5 +342,8 @@
     <br>
     <!--Lineas de conexion a la libreria de materialize-->
     <script type="text/javascript" src="js/materialize.min.js"></script>
+    <script>
+        M.AutoInit();
+    </script>
 </body>
 </html>
